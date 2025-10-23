486257e2c08eeeb512aca39c97c254b3f181c9d9
Ramadhan Galgalo <ramadhan.galgalo@student.moringaschool.com>
2025-10-23 18:42:58 +0300

    added event listeners to DOM elements

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
+
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  function handleFocus(e) {
+    console.log("Good!");
+  }
+
+  function handleBlur(e) {
+    console.log("Hey! Eyes on me!");
+  }
+
+  return (
+    <button onBlur={handleBlur} onFocus={handleFocus}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,16 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    function handleChange(e) {
+        e.preventDefault();
+        console.log("Entering password...")
+    }
+
     return (
-        <div></div>
+        <div>
+            <input onChange={handleChange}type="password"></input>
+        </div>
     )
 }
 
